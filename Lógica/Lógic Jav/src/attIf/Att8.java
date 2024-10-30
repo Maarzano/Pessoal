32975ed9f4b5c88b550fd2985e0f0ad56d5d0a2a
Marzano <arthuramarzano10@gmail.com>
2024-10-30 17:08:08 -0300

    fazendo atividades de If/else

---
+++ Lógica/Lógic Jav/src/attIf/Att8.java
@@ -0,0 +1,16 @@
+/*Escreva um programa que receba um valor de venda e calcule o imposto a ser pago. 
+Se o valor for acima de R$ 1.000,00, o imposto é de 20%; se for até R$ 1.000,00, o imposto é de 10%.*/
+package attIf;
+import java.util.Scanner;
+public class Att8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+    
+    
+    
+    
+    
+        scanner.close();
+    }
+}
