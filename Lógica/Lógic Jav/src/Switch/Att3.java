203f1c3b54fccd9a98c4b9269ef991dea28b2038
Marzano <arthuramarzano10@gmail.com>
2024-11-01 19:10:47 -0300

    xuru mexeu nos trem

--- Lógica/Lógic Jav/src/Switch/Att3.java
+++ Lógica/Lógic Jav/src/Switch/Att3.java
@@ -2,30 +2,31 @@ package Switch;
 import java.util.Scanner;
 public class Att3 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("escolha entre: Dolar (D) Euro (E) Libra (L) Yenes (Y) ");
+        System.out.print("escolha entre: Dolar (D) Euro (E) Libra (L) Yenes (Y) ");
             String moeda = scanner.nextLine();
 
-        System.out.println("digite seu valor: ");
+        System.out.print("digite seu valor: ");
             double valor = scanner.nextDouble();
 
         switch (moeda){
             case "D":
             case "Dolar":
-                System.out.println("a converção de real para dolar fica: " + valor/5);
+                System.out.println("a converção de real para dolar fica: " + String.format("%.2f", valor/5));
                 break;
             case "E":
             case "Euro":
-                System.out.println("a converção de real para Euro fica: " + valor/6);
+                System.out.println("a converção de real para Euro fica: " + String.format("%.2f", valor/6));
                 break;
             case "L":
             case "Libra":
-                System.out.println("a converção de real para Libra fica: " + valor/5.5);
+                System.out.println("a converção de real para Libra fica: " + String.format("%.2f", valor/5.5));
                 break;
             case "Y":
             case "Yenes":
-                System.out.println("a converção de real Yenes para Yenes fica: " + (valor * 100)/0.05);
+                System.out.println("a converção de real para Yenes fica: " + String.format("%.2f", (valor * 100)/0.05));
                 break;
             default:
                 System.out.println("digitou número inválido");
