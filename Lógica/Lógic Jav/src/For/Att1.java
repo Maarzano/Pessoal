5d6bb1fbb3edac59b8c2c280b5268c5eccdba3c6
Marzano <arthuramarzano10@gmail.com>
2024-11-09 20:49:05 -0300

    arrumando For

--- Lógica/Lógic Jav/src/For/Att1.java
+++ Lógica/Lógic Jav/src/For/Att1.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class Att1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+
+
+
+        scanner.close();
     }
 
 }
