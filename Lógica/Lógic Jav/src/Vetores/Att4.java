35b7a9a3c4e84d38a58a2456e84520b50fdca3b2
Marzano <80858692+Maarzano@users.noreply.github.com>
2024-11-26 19:21:55 -0300

    Create Att4.java

---
+++ Lógica/Lógic Jav/src/Vetores/Att4.java
@@ -0,0 +1,25 @@
+//soma com vetores
+package Vetores;
+import java.util.Scanner;
+public class Att1 {
+ public static void main(String[] args) {
+  Scanner Scan = new Scanner(System.in);
+   
+  int[] VetSoma = new int[3];
+   
+  System.out.println("escreva o primeiro número inteiro: ");
+  VetSoma[0] = Scan.nextInt();
+   
+  System.out.println("o segundo: ");
+  VetSoma[1] = Scan.nextInt();
+   
+  System.out.println("o terceiro: ");
+  VetSoma[2] = Scan.nextInt();
+   
+  int Soma = VetSoma[0] + VetSoma[1] + VetSoma[2];
+   
+  System.out.println("a soma de seus número é: " + Soma);
+   
+Scan.close();
+ }
+}
