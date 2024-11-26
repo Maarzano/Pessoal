5c15f444d94d2c9e6cb5d722356ad61b4ed72537
Marzano <80858692+Maarzano@users.noreply.github.com>
2024-11-25 21:44:14 -0300

    atividades no curso de vetor

---
+++ Lógica/Lógic Jav/src/Vetores/Teste3.java
@@ -0,0 +1,24 @@
+//Crie um vetor de inteiros e imprima o vetor em ordem invertida.
+public class Teste3 {
+    public static void main(String[] args) {
+
+        int[] Vet = {4, 6, 7, 10};
+        int passando = 0;
+
+        System.out.println("vetor normal: ");
+
+        while (passando < Vet.length) {
+            System.out.println(Vet[passando]);
+        passando++;
+        }
+
+        System.out.println("vetor invertido: ");
+
+        passando -= 1;
+        while (passando >= 0) {
+            System.out.println(Vet[passando]);
+            passando--;
+
+        }
+    }
+}
