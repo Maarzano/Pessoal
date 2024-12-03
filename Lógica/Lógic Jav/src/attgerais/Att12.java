3a90b470c607de5ebb34ff2ec328923a0f46f54d
Marzano <80858692+Maarzano@users.noreply.github.com>
2024-12-03 20:33:40 -0300

    Create Att12.java

---
+++ Lógica/Lógic Jav/src/attgerais/Att12.java
@@ -0,0 +1,27 @@
+//Crie uma pilha de strings e inverta a ordem das palavras usando a pilha.
+import java.util.*;
+public class Att12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		Deque<String> pilhaNome = new ArrayDeque<String>();
+		Deque<String> pilhaNomeInvertida = new ArrayDeque<String>();
+		
+		System.out.println("digite um nome ");
+		String nomeLer = scanner.nextLine();
+		
+		pilhaNome.push("arthur");
+		pilhaNome.push("patrícia");
+		pilhaNome.push("joao");
+		pilhaNome.push(nomeLer);
+		
+		System.out.println(pilhaNome);
+		
+		while (!pilhaNome.isEmpty()){
+		    pilhaNomeInvertida.push(pilhaNome.pop());
+		}
+		
+		
+		System.out.println(pilhaNomeInvertida);
+		
+	}
+}
