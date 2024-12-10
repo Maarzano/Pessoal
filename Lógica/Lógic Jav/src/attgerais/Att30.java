56ae83e03087da131b97cad1e96bdc1827e00754
Marzano <arthuramarzano10@gmail.com>
2024-12-10 12:51:32 -0300

    atividade de mapa

--- Lógica/Lógic Jav/src/attgerais/Att30.java
+++ Lógica/Lógic Jav/src/attgerais/Att30.java
@@ -7,13 +7,24 @@ import java.util.*;
 public class Att30 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String, Integer> palavras = new HashMap<>();
+        Map<String, Integer> palavras = new TreeMap<>();  //uso Treemap para ordenar pelas palavras
+        int pal =0; //variável para contar quantidade total de palavras
 
         System.out.println("Digite seu texto: ");
-        String texto = scanner.nextLine();
+        String texto = scanner.nextLine().toLowerCase();
 
+        texto = texto.replaceAll("[^a-zA-Z\\s]", ""); //retiro pontuações 
 
-        
-        
+        for(String words: texto.split("\\s+")){ // pego o vetor split e itero cada valor na String word
+            pal++; //conto mais uma palavra
+            palavras.merge(words, 1, Integer::sum);// adiciono no mapa
+        }
+
+        for(String words: palavras.keySet()){ //mostro as palavras
+            System.out.println(words +":"+ palavras.get(words));
+        }
+        System.out.println("Total palavras: " + pal);
+        System.out.println("Total de palavras únicas: " + palavras.size());
+        scanner.close();
     }
 }
