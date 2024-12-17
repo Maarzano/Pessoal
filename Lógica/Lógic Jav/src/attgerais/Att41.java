929f537881aec4fce2d1d049ca92b504637c4fef
Marzano <80858692+Maarzano@users.noreply.github.com>
2024-12-17 19:23:41 -0300

    doing another activity

--- Lógica/Lógic Jav/src/attgerais/Att41.java
+++ Lógica/Lógic Jav/src/attgerais/Att41.java
@@ -2,9 +2,62 @@
 
 O primeiro índice do número.
 O último índice do número. */
+
+import java.util.*;
+
 public class Att41 {
     public static void main(String[] args) {
-        
-    }
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = {1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10};
 
+        int inicio = 0, fim = numeros.length - 1, meio;
+        boolean achou = false;
+
+        System.out.println("Qual número você quer procurar?");
+        int procurando = scanner.nextInt();
+
+        int primeiroIndice = -1;
+        int ultimoIndice = -1;
+
+        while (inicio <= fim) {
+            meio = (inicio + fim) / 2;
+
+            if (numeros[meio] < procurando) {
+                inicio = meio + 1;
+            } else if (numeros[meio] > procurando) {
+                fim = meio - 1;
+            } else {
+                achou = true;
+                primeiroIndice = meio;
+
+                fim = meio - 1;
+            }
+        }
+
+        inicio = 0;
+        fim = numeros.length - 1;
+        while (inicio <= fim) {
+            meio = (inicio + fim) / 2;
+
+            if (numeros[meio] < procurando) {
+                inicio = meio + 1;
+            } else if (numeros[meio] > procurando) {
+                fim = meio - 1;
+            } else {
+                achou = true;
+                ultimoIndice = meio;
+
+                inicio = meio + 1;
+            }
+        }
+
+        if (achou) {
+            System.out.println("O primeiro índice do número " + procurando + " é: " + primeiroIndice);
+            System.out.println("O último índice do número " + procurando + " é: " + ultimoIndice);
+        } else {
+            System.out.println("Número não encontrado.");
+        }
+
+        scanner.close();
+    }
 }
