d0503cfde72864de74016ea9598f6c3b78c8bfa1
Marzano <arthuramarzano10@gmail.com>
2025-01-13 10:55:25 -0300

    initialize ConsoleApp1 project with .NET 9.0 framework and necessary configuration files

--- Lógica/Lógic Jav/src/attgerais/att1.java
+++ Lógica/Lógic Jav/src/attgerais/att1.java
@@ -2,7 +2,7 @@
 //1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
 
 import java.util.*;
-public class Att1 {
+public class att1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
