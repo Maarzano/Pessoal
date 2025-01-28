b03a12a4de7cb5032b75c803ddb7d046c7369e86
Marzano <arthuramarzano10@gmail.com>
2025-01-28 13:57:05 -0300

    testing somethings

--- Lógica/Lógic C#/Program.cs
+++ Lógica/Lógic C#/Program.cs
@@ -1,13 +1,4 @@
 ﻿using Lógic_C_.Models;
 
-Pessoa pessoa1 = new Pessoa();
+string apresentacao = "Olá, meu nome é ";
 
-Console.WriteLine("Digite o nome da pessoa:");
-pessoa1.Nome = Console.ReadLine();
-
-Console.WriteLine("Digite a idade da pessoa:");
-pessoa1.Idade = Convert.ToInt32(Console.ReadLine());
-
-pessoa1.Apresentar();
-
-Console.WriteLine($"Você digitou o nome: {pessoa1.Nome}");
--- Lógica/Lógic C#/obj/Debug/net9.0/Lógic C#.AssemblyInfo.cs
+++ Lógica/Lógic C#/obj/Debug/net9.0/Lógic C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lógic C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46ed5a8d30351e4742f8216e5e197227620f5f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c96fe4fd96fa9931654640b8fe9f60762607d9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lógic C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lógic C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
