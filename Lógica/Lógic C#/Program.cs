d46ed5a8d30351e4742f8216e5e197227620f5f9
Marzano <arthuramarzano10@gmail.com>
2025-01-28 10:42:26 -0300

    começando a criar classe Pessoa

--- Lógica/Lógic C#/Program.cs
+++ Lógica/Lógic C#/Program.cs
@@ -1,2 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Oi cario");
+﻿using Lógic_C_.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "João";
+pessoa1.Idade = 20;
+pessoa1.Apresentar();
