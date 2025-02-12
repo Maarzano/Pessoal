c1562bf35d4b517fd09bbc0d7adccf8cb80c5018
Marzano <arthuramarzano10@gmail.com>
2025-02-12 16:10:48 -0300

    implementando sistema de gerenciamento de pessoas e cursos

--- Projetos/AttsC#(POO)/Att2/Program.cs
+++ Projetos/AttsC#(POO)/Att2/Program.cs
@@ -1,5 +1,5 @@
-﻿using Att2.Models;
-
+﻿using System;
+using Att2.Models;
 
 namespace Att2
 {
@@ -7,22 +7,57 @@ namespace Att2
     {
         public static void Main(string[] args)
         {
-            Pessoa pessoa1 = new("Arthur");
-            pessoa1.Idade = 19;
-            pessoa1.Sobrenome = "Marzano";
+            Curso curso = new("Desenvolvimento de Software");
 
-            Pessoa pessoa2 = new();
-            pessoa2.Nome = "Patrícia";
-            pessoa2.Idade = 20;
-            pessoa2.Sobrenome = "Correa";
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Sistema de Gerenciamento de Pessoas e Cursos");
+                Console.WriteLine("1. Adicionar Pessoa");
+                Console.WriteLine("2. Listar Pessoas");
+                Console.WriteLine("3. Criar Curso");
+                Console.WriteLine("4. Listar Alunos do Curso");
+                Console.WriteLine("5. Sair");
+                Console.Write("Escolha uma opção: ");
 
-            Console.WriteLine(pessoa1.NomeCompleto);
-            Console.WriteLine(pessoa2.NomeCompleto);
-
-            Curso curso = new("Sexo");
-
-
-            Console.WriteLine(curso.Nome);
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Write("Nome: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Sobrenome: ");
+                        string sobrenome = Console.ReadLine();
+                        Console.Write("Idade: ");
+                        int idade = int.Parse(Console.ReadLine());
+                        Pessoa pessoa = new(nome) { Sobrenome = sobrenome, Idade = idade };
+                        curso.AdicionarAluno(pessoa);
+                        break;
+                    case "2":
+                        Console.WriteLine("\nLista de Pessoas:");
+                        foreach (Pessoa aluno in curso.Alunos)
+                        {
+                            Console.WriteLine(aluno.NomeCompleto);
+                        }
+                        break;
+                    case "3":
+                        Console.Write("Nome do Curso: ");
+                        string nomeCurso = Console.ReadLine();
+                        curso = new Curso(nomeCurso);
+                        Console.WriteLine($"Curso '{curso.Nome}' criado com sucesso!");
+                        break;
+                    case "4":
+                        curso.ListarAlunos();
+                        break;
+                    case "5":
+                        Console.WriteLine("Saindo...");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+                Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
         }
     }
 }
