98af3b14b2585f6ff561575a93f6d9e31770c5af
Marzano <arthuramarzano10@gmail.com>
2025-04-23 16:54:04 -0300

    tentando fazer desafio proposto pela DIO (impossível devido a péssima didatica do mano da DIO)

---
+++ Lógica/FrontEnd/React/Projeto1/calculadora-com-react/src/App.js
@@ -0,0 +1,14 @@
+import Input from "./components/Input";
+import { Container, Content } from "./styles";
+
+function App() {
+  return (
+    <Container>
+      <Content>
+        <Input/>
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
