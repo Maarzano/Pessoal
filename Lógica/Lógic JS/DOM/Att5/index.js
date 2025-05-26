d2c3fd4ee98acb17e4faad5c3271fda1645dbcbc
Marzano <arthuramarzano10@gmail.com>
2025-05-26 11:46:02 -0300

    mudando de anime para gatos pq tem o cors desabilitado

--- Lógica/Lógic JS/DOM/Att5/index.js
+++ Lógica/Lógic JS/DOM/Att5/index.js
@@ -2,7 +2,10 @@ const button = document.getElementsByClassName("btn")[0];
 
 
 button.addEventListener("click", async () => {
-    const url = "https://api.nekosapi.com/v4/images/random?rating=safe&limit=1";
+    const url = "https://api.thecatapi.com/v1/images/search";
+
+    button.disabled = true;
+    button.innerText = "Buscando...";
 
     fetch(url, {
         method: "GET",
@@ -13,15 +16,14 @@ button.addEventListener("click", async () => {
         return response.json();
     }).then(data => {
         const imageUrl = data[0].url;
-        const autor = (data[0].artist_name == null ? "usuário indisponível" : data[0].artist_name);
         
-
-        document.getElementsByClassName("autor")[0].innerText = autor;
         document.getElementsByClassName("imagem")[0].src = imageUrl;
 
-        console.log(imageUrl);
     }).catch(error => {
         console.error("Erro: ", error);
+    }).then(() =>{
+        button.disabled = false;
+        button.innerText = "Buscar imagem";
     });
 
 });
