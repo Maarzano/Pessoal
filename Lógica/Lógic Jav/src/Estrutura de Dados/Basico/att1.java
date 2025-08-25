8d1dee50f42fb535ea1cf0799f76990cf13f15dc
Marzano <arthuramarzano10@gmail.com>
2025-08-25 11:09:13 -0300

    atualizando atividade 1 com mais dificuldade

--- Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att1.java
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att1.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class att1 {
     public static void main(String[] args) {
-        int numeros[] = {1, 2, 4, 5, 6, 7};
+        Scanner sc = new Scanner(System.in);
+        int[] vetor = new int[10];
+        int maiorValor = 0;
 
-        System.out.println(numeros[2]);
+        System.out.println("Escreva 10 números aí fih");
+
+        for (int i = 0; i < 10; i++){
+            System.out.print(i + 1 +": ");
+            vetor[i] = sc.nextInt();
+            if (vetor[i] > maiorValor) maiorValor = vetor[i];
+        }
+
+        mostrarVetor(vetor);
+        System.out.println("\nO maior número de seu vetor é: " + maiorValor);
+
+    }
+
+    public static void mostrarVetor(int[] vetor){
+        System.out.print("O seu vetor é: ");
+        for(var num : vetor){
+            System.out.print(num + " ");
+        }
     }
 }
