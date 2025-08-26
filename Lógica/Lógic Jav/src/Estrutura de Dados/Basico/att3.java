98ebf3cf600ddb4f7bb84290b7862b8ec91c32be
Marzano <arthuramarzano10@gmail.com>
2025-08-26 10:25:12 -0300

    faz atividade 3

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att3.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import Helpers.Vetores;
+
+public class att3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quer criar um vetor de qual tamanho? ");
+        int resposta = sc.nextInt();
+
+        System.out.println("Então agora preencha ele: ");
+        int[] vetor = Vetores.criarVetorInteiro(resposta);
+
+        int[] numerosPares = Vetores.numerosPares(vetor);
+
+        System.out.println("Os números pares de seu vetor são: " + Arrays.toString(numerosPares));
+    }
+
+}
