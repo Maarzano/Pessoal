28c1fd26b1438ddc4970305ffa9da00c65376256
Marzano <arthuramarzano10@gmail.com>
2025-09-03 12:30:44 -0300

    testes com o Verray

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/VetorLikeArray.java
@@ -0,0 +1,10 @@
+import Helpers.Verray;
+
+public class VetorLikeArray {
+    public static void main(String[] args) {
+        Verray teste = new Verray(10);
+        teste.add(10);
+
+        System.out.println(teste.toString());
+    }
+}
