5c8508ab6cea9657f6d7c88bddc8024670149b18
Marzano <arthuramarzano10@gmail.com>
2025-09-09 11:08:44 -0300

    faz atividade de revisão de linked-list

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att12.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class att12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean continuarLoop = true;
+        LinkedList<String> lista = new LinkedList<>();
+        while(continuarLoop){ 
+            System.out.println("1. Adicionar pessoas na lista\n2. ver o 1° da lista\n3. ver o último da lista\n4. Sair");
+            String escolha = sc.nextLine();
+
+            switch (escolha) {
+                case "1":
+                    System.out.print("Escreva o nome da pessoa: ");
+                    lista.add(sc.nextLine());
+                break;
+                case "2":
+                    if (!lista.isEmpty()){
+                        System.out.println(lista.getFirst());
+                    } else {
+                        System.out.println("A lista está vazia");
+                    }
+                    
+                break;
+                case "3":
+                    if (!lista.isEmpty()){
+                        System.out.println(lista.getLast());
+                    } else {
+                        System.out.println("A lista está vazia");
+                    }
+                break;
+                case "4":
+                    continuarLoop = false;
+                break;
+            
+                default:
+                    System.out.println("Opção errada tente novamente");
+                    continue;
+            }
+        }
+    }
+
+}
