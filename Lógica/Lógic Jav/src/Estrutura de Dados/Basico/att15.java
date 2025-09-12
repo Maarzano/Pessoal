450053e72d5c9a5a7a34fbba6ecc3962b925158e
Marzano <arthuramarzano10@gmail.com>
2025-09-12 12:05:21 -0300

    faz ativida 15 de encontrar o númeor e retornar o índice dele

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att15.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import Helpers.Vetores;
+
+public class att15 {
+    public static void main(String[] args) {
+        System.out.println("Escreva um número: ");
+        Scanner sc = new Scanner(System.in);
+        int numeroALvo = sc.nextInt();
+
+        int[] vetor = Vetores.criarVetorInteiro(10);
+
+        int indiceNumero = Vetores.existeNumeroIndice(vetor, numeroALvo);
+
+        System.out.println(indiceNumero != -1 ? "Econtrado no índice: " + indiceNumero : "Número não encontrado");
+    }
+
+}
