47f14537cbfd0b1835dd73724c51914b8f552d9e
Marzano <arthuramarzano10@gmail.com>
2025-09-12 16:41:14 -0300

    faz exercício 18 de matriz

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att18.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import Helpers.Matrizes;
+
+public class att18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("fale um número: ");
+        int numero = sc.nextInt();
+
+        int[][] matriz = Matrizes.criarMatriz2(numero, numero, sc);
+
+        Matrizes.imprimirMatriz2(matriz);
+        System.out.println("\nA soma dos números do vetor é " + Matrizes.somarMatriz2(matriz));
+    }
+
+}
