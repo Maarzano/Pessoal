6b8f362dbc79896af9b1b0c968cbad315efe02ca
Marzano <arthuramarzano10@gmail.com>
2025-09-23 19:16:20 -0300

    faz atividade 28 de Set

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att28.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import Helpers.Vetores;
+
+public class att28 {
+    public static void main(String[] args) {
+        int[] lista = new int[]{1, 2, 3, 4, 5, 6, 7, 7, 7, 5, 3, 2, 2};
+
+        System.out.println("Temos uma lista onde tem duplicatas: ");
+        Vetores.mostrarVetor(lista);
+
+        System.out.println("Agora vou mostrar sem as duplicatas");
+
+        Set<Integer> conjunto = new HashSet<>();
+
+        for (int numero : lista) conjunto.add(numero);
+
+        for (int valor : conjunto) System.out.print(valor + " ");
+       
+    }
+
+}
