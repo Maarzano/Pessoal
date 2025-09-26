d23a0e7dda91effd0577ee7b222a2e5f8d73b8b2
Marzano <arthuramarzano10@gmail.com>
2025-09-26 11:39:55 -0300

    faz atividade 37 de revisão de ampas e vetores para ver notas de alunos

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att37.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import Helpers.Vetores;
+
+public class att37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String, int[]> mapa = new HashMap<>();
+
+        for (int i = 0; i < 5;i++){
+            System.out.println("Digite o nome do estudante: ");
+            String estudante = sc.nextLine();
+            System.out.println("Você agora vai escrever as notas de " + estudante);
+
+            mapa.put(estudante, Vetores.criarVetorInteiro(5));
+            
+        }
+
+        for (Map.Entry<String, int[]> entry : mapa.entrySet()){
+            System.out.print("\nAluno " + entry.getKey() + " tem as notas ");
+            for (var nota : entry.getValue()){
+                System.out.print(nota + " ");
+            }
+        }
+        sc.close();
+    }
+
+}
