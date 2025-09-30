0bd3603b02a783e21344e78a67f28949894b8146
Marzano <arthuramarzano10@gmail.com>
2025-09-30 15:59:37 -0300

    faz atividade 42 de hashMap

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att42.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import Helpers.Vetores;
+
+public class att42 {
+    public static void main(String[] args) {
+        int[] vetor = Vetores.criarVetorInteiro(5);
+        HashMap<Integer, List<Integer>> mapa = new HashMap<>();
+
+        for (int n : vetor){
+            int index = n % 3;
+            mapa.putIfAbsent(index,new ArrayList<>());
+
+            mapa.get(index).add(n);
+        }
+        System.out.println(mapa);
+    }
+
+}
