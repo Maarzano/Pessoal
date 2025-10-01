5ab24a46c623b459ecd8a93e977d8152198c576e
Marzano <arthuramarzano10@gmail.com>
2025-10-01 12:17:19 -0300

    faz atividade de stack e lista ligada

---
+++ Lógica/Lógic Jav/src/Estrutura de Dados/Basico/att44.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+import MinhasEstruturas.LinkedList.Single.SinglyLinkedList;
+
+public class att44 {
+    public static void main(String[] args) {
+
+        SinglyLinkedList<Integer> listapronta = inverterPrimeirosK(new SinglyLinkedList<>(new Integer[]{1, 2, 3, 4, 5}), 2);
+
+        listapronta.printList();
+        
+    }
+
+    public static <T> SinglyLinkedList<T> inverterPrimeirosK(SinglyLinkedList<T> lista, int k){
+        Stack<T> pilha = new Stack<>();
+        SinglyLinkedList<T> resultado = new SinglyLinkedList<>();
+
+        int count = 0;
+    for (T valor : lista) {
+        if (count < k) {
+            pilha.push(valor);
+        } else {
+            while (!pilha.isEmpty()) {
+                resultado.add(pilha.pop());
+            }
+            resultado.add(valor);
+        }
+        count++;
+    }
+
+    while (!pilha.isEmpty()) {
+        resultado.add(pilha.pop());
+    }
+
+    return resultado;
+    }
+
+}
