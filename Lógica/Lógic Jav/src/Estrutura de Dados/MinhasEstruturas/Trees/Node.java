d95ec69dfaf539cb05ddaa4d5d4611ffb9d63fdf
Marzano <arthuramarzano10@gmail.com>
2025-10-08 17:20:55 -0300

    começo da implementação

--- Lógica/Lógic Jav/src/Estrutura de Dados/MinhasEstruturas/Trees/Node.java
+++ Lógica/Lógic Jav/src/Estrutura de Dados/MinhasEstruturas/Trees/Node.java
@@ -1,5 +1,15 @@
 package MinhasEstruturas.Trees;
 
-public class Node {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node<T> {
+    T valor;
+    List<Node<T>> filhos;
+
+    Node(T valor){
+        this.valor = valor;
+        filhos = new ArrayList<>();
+    }
 
 }
