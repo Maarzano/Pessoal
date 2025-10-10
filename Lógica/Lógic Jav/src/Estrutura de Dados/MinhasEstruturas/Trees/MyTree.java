51042fbf2bb1f740353addbd9f9a1535bed47e67
Marzano <arthuramarzano10@gmail.com>
2025-10-10 11:20:08 -0300

    feat(MyTree) inicia construtor com método de add na minha árvore

--- Lógica/Lógic Jav/src/Estrutura de Dados/MinhasEstruturas/Trees/MyTree.java
+++ Lógica/Lógic Jav/src/Estrutura de Dados/MinhasEstruturas/Trees/MyTree.java
@@ -1,5 +1,15 @@
 package MinhasEstruturas.Trees;
 
-public class MyTree {
+public class MyTree<T> {
+    Node<T> root;
+
+    public MyTree(T valorRaiz) {
+        root = new Node<>(valorRaiz);
+    }
+
+    public void add(Node<T> no, T valor){
+        Node<T> filho = new Node<>(valor);
+        no.filhos.add(filho);
+    }
 
 }
