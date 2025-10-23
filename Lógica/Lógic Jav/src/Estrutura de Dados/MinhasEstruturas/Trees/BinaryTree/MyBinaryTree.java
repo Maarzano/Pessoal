52380328e1c0e0766e9247b4dcf72f7ba953d551
Marzano <arthuramarzano10@gmail.com>
2025-10-23 11:24:26 -0300

    adiciona métodos de comparação e de iteração e também de procura na MyBinaryTree

--- Lógica/Lógic Jav/src/Estrutura de Dados/MinhasEstruturas/Trees/BinaryTree/MyBinaryTree.java
+++ Lógica/Lógic Jav/src/Estrutura de Dados/MinhasEstruturas/Trees/BinaryTree/MyBinaryTree.java
@@ -1,6 +1,10 @@
 package MinhasEstruturas.Trees.BinaryTree;
 
-public class MyBinaryTree<T> {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+public class MyBinaryTree<T extends Comparable<T>> implements Iterable<T>{
     Node<T> root;
 
     public MyBinaryTree(T valor){
@@ -11,8 +15,63 @@ public class MyBinaryTree<T> {
         this.root = new Node<>(null);
     }
 
-/*  public boolean Search(T valor){
-        TODO implementar
-    }*/
+    public boolean search(T valor) {
+        if (root == null || root.valor == null) {
+            return false;
+        }
+
+        Node<T> current = root;
+        while (current != null) {
+            int comparacao = valor.compareTo(current.valor);
+
+            if (comparacao == 0) {
+                return true;
+            } else if (comparacao < 0) {
+                current = current.esquerdo;
+            } else {
+                current = current.direito;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Stack<Node<T>> stack = new Stack<>();
+
+            {
+                pushLeftChildren(root);
+            }
+
+            @Override
+            public boolean hasNext() {
+                return !stack.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("A árvore não tem mais elementos.");
+                }
+
+                Node<T> nodeToReturn = stack.pop();
+
+                if (nodeToReturn.direito != null) {
+                    pushLeftChildren(nodeToReturn.direito);
+                }
+
+                return nodeToReturn.valor;
+            }
+
+            private void pushLeftChildren(Node<T> node) {
+                while (node != null) {
+                    stack.push(node);
+                    node = node.esquerdo;
+                }
+            }
+        };
+    }
 
 }
