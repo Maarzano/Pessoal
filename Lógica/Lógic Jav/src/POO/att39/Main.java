bb804771c033076cf0bcabe2e6e6cf5c6eae8921
Marzano <arthuramarzano10@gmail.com>
2025-12-17 13:50:56 -0300

    cria main para testar

---
+++ Lógica/Lógic Jav/src/POO/att39/Main.java
@@ -0,0 +1,11 @@
+package att39;
+
+import att39.models.Banco;
+
+public class Main {
+    public static void main(String[] args) {
+        Banco banco = new Banco("admin");
+        banco.acessar();
+    }
+
+}
